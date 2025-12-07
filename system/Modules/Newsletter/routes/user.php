1730242be457850e427b022bfd34cc8880d3e4f3
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2025-12-07 11:05:07 +0545

    user subscriber update

--- system/Modules/Newsletter/routes/user.php
+++ system/Modules/Newsletter/routes/user.php
@@ -5,3 +5,4 @@ use Modules\Newsletter\App\Http\Controllers\NewsletterController;
 
 Route::middleware(['cors', 'json.response'])->post('/subscribers', [NewsletterController::class, 'store']);
 
+// subscribers routes protected by auth:admin middleware
